972d50d8b512556eb6121babb6efa4a49d0fd3ec
HackCieux <36549922+HackCieux@users.noreply.github.com>
2019-01-20 11:44:39 +0530

    create portscan.sh
    A bash script to check alive hosts and ports

---
+++ portscan.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#populate our variables from the arguments
+
+host=$1
+startport=$2
+stopport=$3
+
+#function pingcheck
+#ping a device to see if it is up
+
+function pingcheck
+{
+ping=`ping -c 1 $host | grep bytes | wc -l`
+if [ "$ping" -gt 1 ];then
+echo "$host is up";
+else
+echo "$host is down quitting";
+exit
+fi
+}
+
+#function portcheck
+#test a port to see if it is open
+
+function portcheck
+{
+for ((counter=$startport; counter<=$stopport; counter++))
+do
+(echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter
+open"
+done
+}
+
+#run our functions
+
+pingcheck
+portcheck
